4b21d1fbef7d4cfba26664eac29c3477f5df9cd9
wazhee <jtrexss@gmail.com>
2025-02-11 00:29:51 -0600

    fixed video capture script

--- video_capture.py
+++ video_capture.py
@@ -2,3 +2,13 @@ import cv2
 
 cv2.namedWindow("preview")
 cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()  
+    # Process the frame using OpenCV functions
+    cv2.imshow('Live Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to quit
+        break
+        
+cap.release()
+cv2.destroyAllWindows()
